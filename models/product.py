a969e0521aed462f527d6aeb46a5021f293fbc1c
eerinessofsilence <eerinessbusiness@gmail.com>
2026-01-04 17:49:29 +0200

    feat(models): add product module

---
+++ models/product.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class Product:
+    name: str
+    description: str
+    category: str
+    brand: int
+    size: int
+    price: int
+
+    translation_enabled: bool = True
+    condition: str = "NEW"
+    amount: int = 1
+    selling_condition: str = "SALE"
+
+    additional_sizes: list[int] = field(default_factory=list)
+    characteristics: list[int] = field(default_factory=list)
+    colors: list[str] = field(default_factory=lambda: ["WHITE"])
+    keywords: list[str] = field(default_factory=list)
